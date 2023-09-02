3c714dc75c53fd96619f2768ce7997512e543aff
simplystudios <new.anshwadhwa@gmail.com>
2023-09-02 22:11:07 +0530

    spinning pfp from github api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{ useEffect, useState } from 'react'
 
 function App() {
+  const [Data,setData] = useState([])
+  const [repData,setrepData] = useState([])
+  useEffect(()=>{
+    get()
+    fetch("https://api.github.com/users/simplystudios").then((result)=>{
+    result.json().then((resp)=>{
+      setData(resp)
+    })
+  })
+  })
+  const get = () =>{
+    return(
+      fetch("https://api.github.com/users/simplystudios/repos").then((result)=>{
+      result.json().then((resp)=>{
+        setrepData(resp)
+      })
+    })
+    )  
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={Data.avatar_url}/>
+        <h1 id='name'>{Data.name}</h1>
+        <h3>{Data.login}</h3>
       </header>
+      <div className='repo'>
+          <h1>{repData.html_url}</h1>
+      </div>
     </div>
   );
 }
